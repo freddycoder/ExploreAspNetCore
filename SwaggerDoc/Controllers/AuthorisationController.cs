fdaaad0bc1031dc2f0adf8e3779554929c23e9e5
Frédéric Jacques <jacqu288@hotmail.ca>
2020-11-08 09:41:40 -0500

    setup https

--- SwaggerDoc/Controllers/AuthorisationController.cs
+++ SwaggerDoc/Controllers/AuthorisationController.cs
@@ -1,18 +1,18 @@
-using Microsoft.AspNetCore.Mvc;
+ï»¿using Microsoft.AspNetCore.Mvc;
 using static SwaggerDoc.Enveloppe.ApiEnveloppeFactory;
 using Microsoft.AspNetCore.Authorization;
 
 namespace SwaggerDoc.Controllers
 {
     /// <summary>
-    /// Contrôlleur pour tester l'authentification
+    /// ContrÃ´lleur pour tester l'authentification
     /// </summary>
     [ApiController]
     [Route("[controller]/[action]")]
     public class AuthorisationController : Controller
     {
         /// <summary>
-        /// Retourne true si autorisé, sinon 403
+        /// Retourne true si autorisÃ©, sinon 403
         /// </summary>
         /// <returns></returns>
         [HttpGet]
